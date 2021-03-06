f23a6fed3f55de77b3b7492c896923568bd64db4
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-03-06 09:28:37 +0300

    Update PRAC1_Class.cpp

--- PRAC1_Class.cpp
+++ PRAC1_Class.cpp
@@ -1,4 +1,4 @@
-//valgrind --leak-check=full -v -q --trace-children=yes --track-fds=yes ./executable
+
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -21,11 +21,7 @@ public:
 
     Student(const char * name="UNDEFINED", long number=0, int day=0, int month=0, int year=0): 
     _number(number), _date_year(year), _date_month(month), _date_day(day)
-    {
-        int i=0;
-        while (name[i] != '\0') {_name[i] = name[i]; i++;}
-        _name[i] = '\0';
-    }
+    {   strcpy(_name, name);   }
 
     friend void StSwap(Student &a, Student &b);
 
@@ -67,97 +63,97 @@ public:
             _students[i] = sts[i];
     }
 
-    void AddSt(Group & g, Student s)
+    void AddSt(Student s)
     {
         int i;
-        for (i=0; i<g._size; i++)
-            if (strcmp(s._name, g._students[i]._name) == 0) return;
-        g._students = (Student *)realloc(g._students, sizeof(Student)*(g._size+2));
-        g._students[g._size]=s;
-        g._size+=1;           //cout << g._size;
+        for (i=0; i<_size; i++)
+            if (strcmp(s._name, _students[i]._name) == 0) return;
+        _students = (Student *)realloc(_students, sizeof(Student)*(_size+2));
+        _students[_size]=s;
+        _size+=1;           //cout << g._size;
     }
 
-    void RemoveSt(Group & g, const char * name)
+    void RemoveSt(const char * name)
     {
         int i;
-        for (i=0; i<g._size; i++)
-            if (strcmp(name, g._students[i]._name) == 0)
+        for (i=0; i<_size; i++)
+            if (strcmp(name, _students[i]._name) == 0)
             {
-                for (; i<g._size-1; i++) g._students[i]=g._students[i+1];
-                Student tmp;    g._students[--g._size] = tmp;   return;
+                for (; i<_size-1; i++) _students[i]=_students[i+1];
+                Student tmp;    _students[--_size] = tmp;   return;
             }
         cout << "\t Student for removal " << name << " not found! \n";         
     }
 
-    void ChangeSt(Group & g, Student s)
+    void ChangeSt(Student s)
     {
         int i;
-        for (i=0; i<g._size; i++)
-            if (strcmp(s._name, g._students[i]._name) == 0)
+        for (i=0; i<_size; i++)
+            if (strcmp(s._name, _students[i]._name) == 0)
             {
-                g._students[i]._number = s._number;     g._students[i]._date_day = s._date_day;     g._students[i]._date_month = s._date_month;     g._students[i]._date_year = s._date_year;
+                _students[i]._number = s._number;     _students[i]._date_day = s._date_day;     _students[i]._date_month = s._date_month;     _students[i]._date_year = s._date_year;
                 cout << "\t Student " << s._name << " found and changed! \n";   return;
             }
         cout << "\t Student " << s._name << " not found! \n";  return;
     } 
 
-    void NameSort(Group & g)
+    void NameSort()
     {
         int i,j;
-        for (i=0; i<g._size; i++)
+        for (i=0; i<_size; i++)
             for (j=0; j<i; j++)
-                if (strcmp(g._students[i]._name, g._students[j]._name) < 0) StSwap(g._students[i], g._students[j]);
+                if (strcmp(_students[i]._name, _students[j]._name) < 0) StSwap(_students[i], _students[j]);
         cout << "NameSort done. \n";
     }
 
-    void DateSort(Group & g)
+    void DateSort()
     {
         int i,j;
-        for (i=0; i<g._size; i++)
+        for (i=0; i<_size; i++)
             for (j=0; j<i; j++)
-                if (g._students[i]._date_year < g._students[j]._date_year) StSwap(g._students[i], g._students[j]);  
-                else if ((g._students[i]._date_year == g._students[j]._date_year) and (g._students[i]._date_month < g._students[j]._date_month)) StSwap(g._students[i], g._students[j]);
-                     else if ((g._students[i]._date_month == g._students[j]._date_month) and (g._students[i]._date_day < g._students[j]._date_day)) StSwap(g._students[i], g._students[j]); 
+                if (_students[i]._date_year < _students[j]._date_year) StSwap(_students[i], _students[j]);  
+                else if ((_students[i]._date_year == _students[j]._date_year) and (_students[i]._date_month < _students[j]._date_month)) StSwap(_students[i], _students[j]);
+                     else if ((_students[i]._date_month == _students[j]._date_month) and (_students[i]._date_day < _students[j]._date_day)) StSwap(_students[i], _students[j]); 
         cout << "DateSort done. \n";
     }
 
-    void NumberSort(Group & g)
+    void NumberSort()
     {
         int i,j;
-        for (i=0; i<g._size; i++)
+        for (i=0; i<_size; i++)
             for (j=0; j<i; j++)
-                if (g._students[i]._number < g._students[j]._number) StSwap(g._students[i], g._students[j]);
+                if (_students[i]._number < _students[j]._number) StSwap(_students[i], _students[j]);
         cout << "NumberSort done. \n";
     }    
 
-    Student NameSeek(Group & g, const char * name) const
+    Student NameSeek(const char * name) const
     {
         int i;
-        for (i=0; i<g._size; i++)
-            if (strcmp(name, g._students[i]._name) == 0)
-            { cout << "\t Student found! \n" << g._students[i]; return(g._students[i]); }
+        for (i=0; i<_size; i++)
+            if (strcmp(name, _students[i]._name) == 0)
+            { cout << "\t Student found! \n" << _students[i]; return(_students[i]); }
         cout << "\t Student " << name << " not found! \n";
         Student st;    return(st);
     }
 
-    Student DateSeek(Group & g, int day, int month, int year) const
+    Student DateSeek(int day, int month, int year) const
     {
         int i;
-        for (i=0; i<g._size; i++)
-            if (day == g._students[i]._date_day)
-                if (month == g._students[i]._date_month)
-                    if (year == g._students[i]._date_year)
-                        { cout << "\t Student found! \n" << g._students[i]; return(g._students[i]); }
+        for (i=0; i<_size; i++)
+            if (day == _students[i]._date_day)
+                if (month == _students[i]._date_month)
+                    if (year == _students[i]._date_year)
+                        { cout << "\t Student found! \n" << _students[i]; return(_students[i]); }
         cout << "\t Student with such date not found! \n";
         Student st;    return(st);
     }
 
-    Student NumberSeek(Group & g, long number) const
+    Student NumberSeek(long number) const
     {
         int i;
-        for (i=0; i<g._size; i++)
-            if (number == g._students[i]._number)
-                { cout << "\t Student found! \n" << g._students[i]; return(g._students[i]); }
+        for (i=0; i<_size; i++)
+            if (number == _students[i]._number)
+                { cout << "\t Student found! \n" << _students[i]; return(_students[i]); }
         cout << "\t Student with such number not found! \n";
         Student st;    return(st);
     }
@@ -192,7 +188,7 @@ int main()
             st2("ОКОНИШНИКОВ", 89853000000, 2, 4, 2002),
             st3("ГЛАДЫШЕВ", 89152000000, 10, 10, 2001);
     //cout << st0 << st1 << st2 << st3;
-    Group gr214(2,3,0); gr214.AddSt(gr214,st0); gr214.AddSt(gr214,st1);  gr214.AddSt(gr214,st2);  gr214.AddSt(gr214,st3);
+    Group gr214(2,3,0); gr214.AddSt(st0); gr214.AddSt(st1);  gr214.AddSt(st2);  gr214.AddSt(st3);
 
     cout << "\n\t \x1b[31mДобро пожаловать в редактор Группы 214! \n     Первоначально в группе 4 участника, с заполненными полями имени, номера и даты рождения соответственно. \x1b[0m\n"; menu();
     for(;;)
@@ -203,15 +199,15 @@ int main()
 
         if (!strcmp("see", str)) cout << gr214;
         else if (!strcmp("menu", str)) menu();
-        else if (!strcmp("sortn", str)) gr214.NameSort(gr214);
-        else if (!strcmp("sortb", str)) gr214.DateSort(gr214);
-        else if (!strcmp("sortp", str)) gr214.NumberSort(gr214);
-        else if (!strcmp("findn", str)) {cout << " -> Введите имя: "; std::cin >> str; gr214.NameSeek(gr214, str);}
-        else if (!strcmp("findb", str)) {cout << " -> Введите дату рождения: Год: "; std::cin >> year; cout << " -> Месяц: "; std::cin >> month; cout << " -> День: "; std::cin >> day; gr214.DateSeek(gr214, day, month, year);}
-        else if (!strcmp("findp", str)) {cout << " -> Введите номер телефона: "; std::cin >> phone; gr214.NumberSeek(gr214, phone);}
-        else if (!strcmp("add", str)) {cout << " -> Введите имя: "; std::cin >> str; cout << " -> Введите номер телефона: "; std::cin >> phone; cout << " -> Введите дату рождения: Год: "; std::cin >> year; cout << " -> Месяц: "; std::cin >> month; cout << " -> День: "; std::cin >> day; Student st(str, phone, day, month, year); gr214.AddSt(gr214, st);}
-        else if (!strcmp("change", str)) {cout << " -> Введите имя: "; std::cin >> str; cout << " -> Введите номер телефона: "; std::cin >> phone; cout << " -> Введите дату рождения: Год: "; std::cin >> year; cout << " -> Месяц: "; std::cin >> month; cout << " -> День: "; std::cin >> day; Student st(str, phone, day, month, year); gr214.ChangeSt(gr214, st);}
-        else if (!strcmp("remove", str)) {cout << " -> Введите имя: "; std::cin >> str; gr214.RemoveSt(gr214, str);}
+        else if (!strcmp("sortn", str)) gr214.NameSort();
+        else if (!strcmp("sortb", str)) gr214.DateSort();
+        else if (!strcmp("sortp", str)) gr214.NumberSort();
+        else if (!strcmp("findn", str)) {cout << " -> Введите имя: "; std::cin >> str; gr214.NameSeek(str);}
+        else if (!strcmp("findb", str)) {cout << " -> Введите дату рождения: Год: "; std::cin >> year; cout << " -> Месяц: "; std::cin >> month; cout << " -> День: "; std::cin >> day; gr214.DateSeek( day, month, year);}
+        else if (!strcmp("findp", str)) {cout << " -> Введите номер телефона: "; std::cin >> phone; gr214.NumberSeek(phone);}
+        else if (!strcmp("add", str)) {cout << " -> Введите имя: "; std::cin >> str; cout << " -> Введите номер телефона: "; std::cin >> phone; cout << " -> Введите дату рождения: Год: "; std::cin >> year; cout << " -> Месяц: "; std::cin >> month; cout << " -> День: "; std::cin >> day; Student st(str, phone, day, month, year); gr214.AddSt(st);}
+        else if (!strcmp("change", str)) {cout << " -> Введите имя: "; std::cin >> str; cout << " -> Введите номер телефона: "; std::cin >> phone; cout << " -> Введите дату рождения: Год: "; std::cin >> year; cout << " -> Месяц: "; std::cin >> month; cout << " -> День: "; std::cin >> day; Student st(str, phone, day, month, year); gr214.ChangeSt(st);}
+        else if (!strcmp("remove", str)) {cout << " -> Введите имя: "; std::cin >> str; gr214.RemoveSt(str);}
         else if (!strcmp("end", str)) { cout << "Ending!! \n"; break;}
         else cout << "No such cmd \n";
     }
