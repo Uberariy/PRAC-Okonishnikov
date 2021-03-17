edcaa1f97620effefce740085cdfe0505f2b8882
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-03-17 20:27:25 +0300

    Update prac2_matrix.cpp

--- prac2_matrix.cpp
+++ prac2_matrix.cpp
@@ -12,20 +12,22 @@ public:
 
     M (int len, int wid, int st=0)
     {
-        int i, j;
-        if ((len < 0) || (wid < 0)) { cerr << "CONSTR: wrong size\n"; exit(1); }
+        int i, j;        
+        if ((len <= 0) || (wid <= 0)) { cerr << "CONSTR: wrong size\n"; exit(1); }
         else 
+        {
             _A = (int **) new int* [len];
-            for (i; i<len; i++)
+            for (i=0; i<len; i++)
                 _A[i] = (int *) new int [wid];
-            for (j=0; j<wid; j++)   for (i; i<len; i++)   _A[i][j] = st;
+            for (j=0; j<wid; j++)   for (i=0; i<len; i++)   _A[i][j] = st;
             _len = len; _wid = wid;
+        }
     }
 
     ~M() 
     {
         int i, j;
-        for (i; i<_len; i++) delete[] _A[i];
+        for (i=0; i<_len; i++) delete[] _A[i];
         delete[] _A;
     }
 
@@ -40,11 +42,17 @@ public:
        else return(_A[i][j]);
     }
 
-    M (const M & A)
+    M &operator= (const M & A)
     {
         int i, j;
-        if ((_wid != A._wid) || (_len != A._len))  { cerr << "ConstrC: wrong indexes\n"; exit(1); } 
-        else for (j=0; j<_wid; j++)   for (i=0; i<_len; i++)   _A[i][j] = A._A[i][j];
+        for (i=0; i<_len; i++) delete[] _A[i];
+        delete[] _A;
+        _len = A._len; _wid = A._wid;
+        _A = (int **) new int* [_len];
+        for (i=0; i<_len; i++)
+            _A[i] = (int *) new int [_wid];     
+        for (j=0; j<_wid; j++)   for (i=0; i<_len; i++)   _A[i][j] = A._A[i][j];
+        return(*this);
     }
 
     M operator+ (const M & A)
@@ -61,17 +69,27 @@ public:
         else for (j=0; j<_wid; j++)   for (i=0; i<_len; i++)   _A[i][j] -= A._A[i][j];
     } 
 
-    M operator* (const M & A)
+    M &operator* (const M & A)
     {
-        int i, j;
+        int i, j, k;
         if ((_wid != A._len) || (_len != A._wid))  { cerr << "*: wrong indexes\n"; exit(1); }
         else 
         {
-            M B(_len, _len, 0);
-            for (j=0; j<A._len; j++)
-                for (i=0; i<A._wid; i++)
-                    B._A[i][j] += _A[i][j] * A._A[j][i];
-            // Тут остановился
+            M B(_len, _len, 0); //cout << _len;
+            for (i=0; i<_len; i++)
+                for (j=0; j<_len; j++)
+                    for (k=0; k<_wid; k++)
+                        { B._A[i][j] += _A[i][k] * A._A[k][i];}// cout << B._A[i][j] << " ";}
+
+            for (i=0; i<_len; i++) delete[] _A[i];
+            delete[] _A;
+            _wid = _len;
+            _A = (int **) new int* [_len];
+            for (i=0; i<_len; i++)
+                _A[i] = (int *) new int [_wid];            
+
+            for (j=0; j<_len; j++)   for (i=0; i<_len; i++)   {_A[i][j] = B._A[i][j]; }//fprintf(stderr, "a");}
+            return(*this);
         }
     }  
 };
@@ -80,5 +98,20 @@ std::ostream& operator<< (std::ostream &out, const M &A)
 {
     int i, j;
     for (i=0; i<A._len; i++)
-        for (j=0; j<A._wid; j++);
+    {
+        for (j=0; j<A._wid; j++)    out << A._A[i][j] << "\t";
+        out << "\n";
+    }
+    out << "\n";    return(out);
+}
+
+int main()
+{
+    M A(3, 2, 1);
+    M B(2, 3, 2);
+    M C(1, 1, 1);
+    cout << A << B;
+    A = A*B;
+    cout << A;
+    return(0);
 }
