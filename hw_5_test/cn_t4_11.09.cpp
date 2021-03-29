336d5b74a8522aef61d5af022fc365a2fe436418
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-03-29 19:07:41 +0300

    Update cn_t4_11.09.cpp

--- hw_5_test/cn_t4_11.09.cpp
+++ hw_5_test/cn_t4_11.09.cpp
@@ -4,21 +4,41 @@
 using namespace std;
 
 class SumHold {
-    int sum;
+    int sumacc, sumrej;
+    static int allsum;
 
 public:
-    SumHold() {sum = 0;}
-    SumHold& operator<< (int i) {sum += i;  return(*this);}
-    int Get() const {return(sum);}
-    void Write() const { cout << "\t" << sum << "\n";}
+    SumHold() {sumacc = 0;  sumrej = 0;}
+    SumHold& operator<< (int i) 
+    {
+        if (Acceptable(i)) sumacc += i; 
+        else sumrej += i;
+        allsum += i; 
+        return(*this);
+    }
+    int Get() const {return(sumacc);}
+    int GetRejected() const {return(sumrej);}
+    int GetAll() const {return(allsum);}
+    void Write() const { cout << sumacc << "\t" << sumrej << "\n";}
+    void WriteAll() const { cout << allsum << "\n";}
+    virtual bool Acceptable(int i) { return(true); }
 
 };
 
+int SumHold::allsum = 0;
+
+class SumHoldSmall : public SumHold {
+
+    bool Acceptable(int i) override { if ((i <= 10) && (i >= -10)) return(true); else return(false); }
+};
 
 int main() 
 {
-    SumHold sh;
-    sh<<20<<30<<40<<50;
+    SumHoldSmall sh, sh1;
+    sh<<10<<30<<40<<50;
+    sh1<<10<<-5<<40;
     sh.Write();
+    sh1.Write();
+    sh.WriteAll();
     return 0;
 }
