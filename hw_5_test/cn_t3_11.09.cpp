8fb38031e117008e7ec8c91fc2127dd13673d1f7
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-03-29 13:24:37 +0300

    Create cn_t3_11.09.cpp

---
+++ hw_5_test/cn_t3_11.09.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class M {
+    int *obj_;
+    int num_;
+
+public:
+    static int count;
+
+    M(int n) {obj_ = new int;   *obj_ = n; count++;   num_=count;}
+    M() {obj_ = new int;    count++;   num_=count;}
+    int& operator* () const { return(*obj_); }
+    int& operator() (M & m) {
+        M A(0);
+        if (num_ == m.num_)
+        return(*obj_);
+        else 
+        return(*A);
+    }
+    ~M() { delete obj_; }
+
+};
+
+int M::count = 0;
+
+int main() 
+{
+    M m(10), x(20);
+    cout << m(m) << m(x) << x(m) << x(x);
+    m(m) = 30;
+    cout << m(m) << m(x);
+    return 0;
+}
