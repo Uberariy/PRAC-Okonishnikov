1735ad0d45c37236f9a717a2944927c7370a9db5
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-03-29 13:24:43 +0300

    Create cn_t7-t8_09.09.2015.cpp

---
+++ hw_5_test/cn_t7-t8_09.09.2015.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class IntQueue {
+    struct list
+    {
+        int num;
+        struct list *next;
+    } *L;
+    struct list *start;
+    struct list *end;
+    int size;
+
+public:
+
+    struct list *listadd (struct list *L, int A)
+    {
+        if (L == NULL) 
+        {
+            L = (list *) new list;
+            L -> num = A;
+            end = L;
+        }
+        else L -> next  = listadd(L -> next, A);
+    }
+
+    void operator<<= (int A) 
+    {
+        L = listadd(L, A);
+    }
+    int operator>>= (int & A) 
+    {
+
+    }
+    int Amount () {return size;}
+};
+
+int main() 
+{
+    return 0;
+}
