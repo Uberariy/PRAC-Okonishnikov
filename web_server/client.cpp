ca43ced265d2b2da3b3c2899af6eb05f32f21e25
Uberariy <70648019+Uberariy@users.noreply.github.com>
2021-04-11 22:50:26 +0300

    Update client.cpp

--- web_server/client.cpp
+++ web_server/client.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <cstring>
 #include <iostream>
+#include <vector>
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -10,18 +11,43 @@
 #include "socket2.hpp"
 using namespace std;
 
-#define DEFAULTPORT 3666
+#define DEFAULTPORT 1234
 int port;
 
 class Client {
     ClientSocket _Client;
     SocketAddress _Serv;
+    int _exit;
 public:
-    Client(int port) : _Serv(INADDR_LOOPBACK, port), _Client() {}
+    Client(int port) : _Serv(htonl(INADDR_LOOPBACK), port), _Client(), _exit(0) {}
     
     void Run()
     {
-        _Client._Connect
+        try { _Client._Connect(_Serv); }
+        catch(Error E) {
+            cerr << "Client: " << E.GetErr() << " error! (Server does not exist, or address is wrong)\n";
+            exit(2);
+        };
+        sleep(1);
+        cout << "Client: Connected to server" << INADDR_LOOPBACK << ":" << port << "!\n"; 
+        string line;
+        for(;;)
+        {
+            std::getline(std::cin, line);   //cout << "|" << line << "|";
+            _Client._Write(_Serv, line, 0);
+            _Client._Write(_Serv, "\r\n", 0);
+            if (line == "Disconnect") 
+            {
+                cout << "Client: Client Disconected\n";
+                _exit = 1;
+            }
+            if (line == "Close")
+            {
+                cout << "Client: Closing Server, Client Disconected\n";
+                _exit = 1;
+            }
+            if (_exit) break;
+        }
     }
 };
 
